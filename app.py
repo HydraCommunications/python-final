0e63fe485d68d03f12a9c267c890b560798f5284
Jerica <jericahanna04@gmail.com>
2024-10-29 10:42:48 -0500

    fixed it oops

--- app.py
+++ app.py
@@ -2,24 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('home.html')
 
+
 @app.route('/room.html')
 def room():
+    return
+
 
 @app.route('/edit_room.html')
 def edit_room():
     return
 
+
 @app.route('/room_details.html')
 def room_details():
     return
 
+
 @app.route('/supply.html')
 def supply():
     return
+
 
 @app.route('/supply_details.html')
 def supply_details():
