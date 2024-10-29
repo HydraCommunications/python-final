19a2b372ff8642c8202ac39c737d46ba0363bd58
Jerica <jericahanna04@gmail.com>
2024-10-29 10:41:57 -0500

    adding app routes and connections to the html files

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+@app.route('/room.html')
+def room():
+
+@app.route('/edit_room.html')
+def edit_room():
+    return
+
+@app.route('/room_details.html')
+def room_details():
+    return
+
+@app.route('/supply.html')
+def supply():
+    return
+
+@app.route('/supply_details.html')
+def supply_details():
+    return
+
+
 if __name__ == '__main__':
     app.run()
