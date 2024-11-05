0ab6b08d83cae483c8163c34548c03b173488c45
Jerica <jericahanna04@gmail.com>
2024-11-05 11:02:11 -0600

    all of the route tests done

--- testing/RouteTests.py
+++ testing/RouteTests.py
@@ -2,33 +2,32 @@
 
 # Jerica Hanna
 # I'll try writing Route Tests
+# What number do I use
 
 import requests;
 
 def test_index():
     response = requests.get('http://localhost:/1500')
-    assert response.status_code == 400
-    assert response.text == 'Index Route Tests'
+    assert response.status_code == 1500
 
 def edit_room():
-    response = requests.get("edit_room.html")
-    assert response
+    response = requests.get("http://localhost:/edit_room.html")
+    assert response.status_code == 1500
 
 def test_add_rooms():
-    response = requests.get('room.html')
-    assert response
+    response = requests.get('http://localhost:/room.html')
+    assert response.status_code == 1500
 
 
 def room_details():
-    response = requests.get('room_details.html')
-    assert response
+    response = requests.get('http://localhost:/room_details.html')
+    assert response.status_code == 1500
 
 def add_supply():
-    response = requests.get('add_supply.html')
-    assert response
+    response = requests.get('http://localhost:/add_supply.html')
+    assert response.status_code == 1500
 
 # def delete_room():
-
 
 # def tests_get_room_by_id():
 
